448059b2c6d3cc57ec789cb70df0097e0ca88138
Elzo <elzohonoratoneto@gmail.com>
2021-04-19 22:09:43 -0400

    First commit

---
+++ StripePayment/ViewModels/PaymentGatwayPageViewModel.cs
@@ -0,0 +1,206 @@
+﻿using Acr.UserDialogs;
+using PaymentStripeTest.Model;
+using Prism.Commands;
+using Prism.Mvvm;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PaymentStripeTest.ViewModels
+{
+    public class PaymentGatwayPageViewModel : BindableBase
+    {
+        #region Variable
+
+        private CreditCardModel _creditCardModel;
+        private TokenService Tokenservice;
+        private Token stripeToken;
+        private bool _isCarcValid;
+        private bool _isTransectionSuccess;
+        private string _expMonth;
+        private string _expYear;
+        private string _title;
+
+        #endregion Variable
+
+        #region Public Property
+        private string StripeTestApiKey = "pk_test_51IeQGpIQWOdWmcX5uJbNiegeI9O5dGXiJ6LqjyG9Al6eZVHzfxd4sBORe4W4v9kw55qfpz4JmOt3xOq4EU4LS9LQ00mxzZShKg";
+        public string ExpMonth
+        {
+            get { return _expMonth; }
+            set { SetProperty(ref _expMonth, value); }
+        }
+
+        public bool IsCarcValid
+        {
+            get { return _isCarcValid; }
+            set { SetProperty(ref _isCarcValid, value); }
+        }
+
+        public bool IsTransectionSuccess
+        {
+            get { return _isTransectionSuccess; }
+            set { SetProperty(ref _isTransectionSuccess, value); }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public string ExpYear
+        {
+            get { return _expYear; }
+            set { SetProperty(ref _expYear, value); }
+        }
+
+        public CreditCardModel CreditCardModel
+        {
+            get { return _creditCardModel; }
+            set { SetProperty(ref _creditCardModel, value); }
+        }
+
+        #endregion Public Property
+
+        #region Constructor
+
+        public PaymentGatwayPageViewModel()
+        {
+            CreditCardModel = new CreditCardModel();
+            Title = "Card Details";
+        }
+
+        #endregion Constructor
+
+        #region Command
+
+        public DelegateCommand SubmitCommand => new DelegateCommand(async () =>
+        {
+            CreditCardModel.ExpMonth = Convert.ToInt64(ExpMonth);
+            CreditCardModel.ExpYear = Convert.ToInt64(ExpYear);
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+            try
+            {
+                UserDialogs.Instance.ShowLoading("Payment processing..");
+                await Task.Run(async () =>
+                {
+
+                    var Token = CreateToken();
+                    Console.Write("Payment Gateway" + "Token :" + Token);
+                    if (Token != null)
+                    {
+                        IsTransectionSuccess = MakePayment(Token);
+                    }
+                    else
+                    {
+                        UserDialogs.Instance.Alert("Bad card credentials", null, "OK");
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                UserDialogs.Instance.HideLoading();
+                UserDialogs.Instance.Alert(ex.Message, null, "OK");
+                Console.Write("Payment Gatway" + ex.Message);
+            }
+            finally
+            {
+                if (IsTransectionSuccess)
+                {
+                    Console.Write("Payment Gateway" + "Payment Successful ");
+
+                    UserDialogs.Instance.HideLoading();
+                }
+                else
+                {
+
+                    UserDialogs.Instance.HideLoading();
+                    UserDialogs.Instance.Alert("Oops, something went wrong", "Payment failed", "OK");
+                    Console.Write("Payment Gateway" + "Payment Failure ");
+                }
+            }
+
+        });
+
+        #endregion Command
+
+        #region Method
+
+        private string CreateToken()
+        {
+            try
+            {
+                StripeConfiguration.SetApiKey(StripeTestApiKey);
+                var service = new ChargeService();
+                var Tokenoptions = new TokenCreateOptions
+                {
+                    Card = new CreditCardOptions
+                    {
+                        Number = CreditCardModel.Number,
+                        ExpYear = CreditCardModel.ExpYear,
+                        ExpMonth = CreditCardModel.ExpMonth,
+                        Cvc = CreditCardModel.Cvc,
+                        Name = "John Tester",
+                        AddressLine1 = "123",
+                        AddressLine2 = "SpringField",
+                        AddressCity = "Welland",
+                        AddressZip = "000000",
+                        AddressState = "Ontario",
+                        AddressCountry = "Canada",
+                        Currency = "cad",
+                    }
+                };
+
+                Tokenservice = new TokenService();
+                stripeToken = Tokenservice.Create(Tokenoptions);
+                return stripeToken.Id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool MakePayment(string token)
+        {
+            try
+            {
+                StripeConfiguration.SetApiKey("sk_test_51IeQGpIQWOdWmcX55lDsQ7T3e9urvvh5C5op4lBz2hmxaWPE6VJX1OdoqKrCHAWPkoesbMhlYygtuNFlsJaXJ4BJ00jiUMhfbU");
+                var options = new ChargeCreateOptions
+                {
+                    Amount = (long)float.Parse("100"),
+                    Currency = "cad",
+                    Description = "Charge for JonTest@example.com",
+                    Source = stripeToken.Id,
+                    StatementDescriptor = "Customer description",
+                    Capture = true,
+                    ReceiptEmail = "elzohonoratoneto@gmail.com",
+                };
+                //Make Payment
+                var service = new ChargeService();
+                Charge charge = service.Create(options);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Payment Gatway (CreateCharge)" + ex.Message);
+                throw ex;
+            }
+        }
+
+        private bool ValidateCard()
+        {
+            if (CreditCardModel.Number.Length == 16 && ExpMonth.Length == 2 && ExpYear.Length == 2 && CreditCardModel.Cvc.Length == 3)
+            {
+            }
+            return true;
+        }
+
+        #endregion Method
+    }
+}
